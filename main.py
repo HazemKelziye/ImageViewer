0de69d86710c7df0ba6ba522be7b3aaec28119d3
Hazem Kelziye <flashrassengan4@gmail.com>
2022-03-23 12:30:54 +0300

    Only getting the pixel values is left

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ import os
 # cv2.IMREAD_UNCHANGED to open the image as it is
 # include the image file in the project folder
 
-img1 = cv2.imread("images/Mecca.jpeg", cv2.IMREAD_UNCHANGED)
-img2 = cv2.imread("images/Testt.png", cv2.IMREAD_UNCHANGED)
-img3 = cv2.imread("images/KohiCup.png", cv2.IMREAD_UNCHANGED)
-img4 = cv2.imread("images/ConfCat.jpeg", cv2.IMREAD_UNCHANGED)
+img1 = cv2.imread("images/Rubik1.jpeg", cv2.IMREAD_UNCHANGED)
+img2 = cv2.imread("images/Rubik2.jpeg", cv2.IMREAD_UNCHANGED)
+img3 = cv2.imread("images/Rubik3.jpeg", cv2.IMREAD_UNCHANGED)
+img4 = cv2.imread("images/Rubik4.jpeg", cv2.IMREAD_UNCHANGED)
 img5 = cv2.imread("images/FamousIm.png", cv2.IMREAD_UNCHANGED)
 
+resized_img1 = cv2.resize(img1, (750,750), interpolation= cv2.INTER_LINEAR)
+resized_img2 = cv2.resize(img2, (750,750), interpolation= cv2.INTER_LINEAR)
+resized_img3 = cv2.resize(img3, (750,750), interpolation= cv2.INTER_LINEAR)
+resized_img4 = cv2.resize(img4, (750,750), interpolation= cv2.INTER_LINEAR)
+resized_img5 = cv2.resize(img5, (750,750), interpolation= cv2.INTER_LINEAR)
+#
 
 # Note!
 # OpenCV uses BGR image format. So, when we read an image using cv2.
@@ -22,22 +28,24 @@ img5 = cv2.imread("images/FamousIm.png", cv2.IMREAD_UNCHANGED)
 # But since we can convert direcrtly from BGR to HSV so...
 
 # Converting from BGR/RGB to HSV/HSI
-hsv_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
-rgb_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+hsv_img1 = cv2.cvtColor(resized_img1, cv2.COLOR_BGR2HSV)
+rgb_img1 = cv2.cvtColor(resized_img1, cv2.COLOR_BGR2RGB)
 
-hsv_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
-rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+hsv_img2 = cv2.cvtColor(resized_img2, cv2.COLOR_BGR2HSV)
+rgb_img2 = cv2.cvtColor(resized_img2, cv2.COLOR_BGR2RGB)
 
-hsv_img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2HSV)
-rgb_img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+hsv_img3 = cv2.cvtColor(resized_img3, cv2.COLOR_BGR2HSV)
+rgb_img3 = cv2.cvtColor(resized_img3, cv2.COLOR_BGR2RGB)
 
-hsv_img4 = cv2.cvtColor(img4, cv2.COLOR_BGR2HSV)
-rgb_img4 = cv2.cvtColor(img4, cv2.COLOR_BGR2RGB)
+hsv_img4 = cv2.cvtColor(resized_img4, cv2.COLOR_BGR2HSV)
+rgb_img4 = cv2.cvtColor(resized_img4, cv2.COLOR_BGR2RGB)
 
-hsv_img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2HSV)
-rgb_img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2RGB)
+hsv_img5 = cv2.cvtColor(resized_img5, cv2.COLOR_BGR2HSV)
+rgb_img5 = cv2.cvtColor(resized_img5, cv2.COLOR_BGR2RGB)
 # This creates a GUI window with a title name of your choice
 
+LastListRGB = [rgb_img1, rgb_img2, rgb_img3, rgb_img4, rgb_img5]
+LastListHSV = [hsv_img1, hsv_img2, hsv_img3, hsv_img4, hsv_img5]
 # cv2.imshow("HSV Mecca",hsv_img)
 # cv2.imshow("RGB Mecca", img)
 
@@ -50,8 +58,8 @@ rgb_img5 = cv2.cvtColor(img5, cv2.COLOR_BGR2RGB)
 
 # Accessing a single pixel value in OpenCV
 # In our case we are going to access the Voxel since it has 3-channels
-print(f"This is the value of voxel at (0,0): {img1[0, 0]} for the BGR Image")
-print(f"This is the value of voxel at (0,0): {rgb_img1[0, 0]} for the RGB Image")
+print(f"This is the value of voxel at (0,0): {img1[250, 250]} for the BGR Image")
+#print(f"This is the value of voxel at (0,0): {resized_img1[250, 250]} for the RGB Image")
 print(f"This is the value of voxel at (0,0): {hsv_img1[0, 0]} for the HSV Converted Image")
 
 #Getting my hands dirty with Tkinter
@@ -116,6 +124,27 @@ status.set(0)
 
 print("value of status",status.get())
 
+temp = 0
+
+coorLabel = Label(root, text="Pixel value")
+coorLabel2 = Label(root, text="Pixel value2")
+
+#Only the Size/shape is left to be adjusted
+
+def displayCoordinates(event):
+    global temp
+    temp +=1
+
+    if status.get() == 0:
+        if temp%2 == 1:
+            coorLabel['text'] = "The pixel ("+str(event.x)+","+str(event.y)+") "+"and the value is:"+str(LastListRGB[current][event.x,event.y])
+        else:
+            coorLabel2['text'] = "The pixel ("+str(event.x)+","+str(event.y)+") "+"and the value is:"+str(LastListRGB[current][event.x,event.y])
+    else:
+        if temp%2 == 1:
+            coorLabel['text'] = "The pixel ("+str(event.x)+","+str(event.y)+") "+"and the value is:"+str(LastListHSV[current][event.x,event.y])
+        else:
+            coorLabel2['text'] = "The pixel ("+str(event.x)+","+str(event.y)+") "+"and the value is:"+str(LastListHSV[current][event.x,event.y])
 
 
 def clicked():
@@ -129,7 +158,10 @@ def clicked():
     else:
         my_label = Label(image=image_list_hsv[current])
 
+    my_label.bind('<Button-1>', displayCoordinates)
     my_label.grid(row=0, column=1)
+    coorLabel.grid(row=7, column=1)
+    coorLabel2.grid(row=8, column=1)
 
 
 Radiobutton(root, text="Convert to HSV domain", variable=status, value=1, command=clicked).grid(row=2, column=1)
@@ -141,6 +173,11 @@ else:
     my_label = Label(image=TkinterHSV1)
 
 my_label.grid(row=0, column=1)
+my_label.bind('<Button-1>', displayCoordinates)
+coorLabel.grid(row=7, column=1)
+coorLabel2.grid(row=8, column=1)
+
+#my_label.bind('<Button-1>', displayCoordinates)
 
 
 def forward(image_number):
@@ -163,7 +200,10 @@ def forward(image_number):
     button_forward = Button(root, text=">>", command=lambda: forward(current))
     button_back = Button(root, text="<<", command=lambda: back(current))
 
+    my_label.bind('<Button-1>', displayCoordinates)
     my_label.grid(row=0, column=1)
+    coorLabel.grid(row=7, column=1)
+    coorLabel2.grid(row=8, column=1)
     button_forward.grid(row=1, column=2)
     button_back.grid(row=1, column=0)
 
@@ -187,8 +227,10 @@ def back(image_number):
     button_forward = Button(root, text=">>", command=lambda: forward(current))
     button_back = Button(root, text="<<", command=lambda: back(current))
 
-
+    my_label.bind('<Button-1>', displayCoordinates)
     my_label.grid(row=0, column=1)
+    coorLabel.grid(row=7, column=1)
+    coorLabel2.grid(row=8, column=1)
     button_forward.grid(row=1, column=2)
     button_back.grid(row=1, column=0)
 
